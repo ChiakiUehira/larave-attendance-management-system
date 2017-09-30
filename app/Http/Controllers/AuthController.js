8acbf5134d4182ad0dcf9cf1ff0b5c7de081b893
in-the-box <hacoco345@gmail.com>
2017-09-30 22:30:57 +0900

    add: eslint

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -5,16 +5,16 @@ class AuthController {
   * login (req, res) {
     try {
       const { email, password } = req.post()
-      const validate = yield req.auth.validate(email, password)
+      yield req.auth.validate(email, password)
       const isAuth = yield req.auth.attempt(email, password)
       if (isAuth) {
-        const user = yield UserModel.query().where('email',email).first()
+        const user = yield UserModel.query().where('email', email).first()
         const token = yield req.auth.generate(user)
-        return res.json({success: true,user,token})
+        return res.json({success: true, user, token})
       }
       return res.json({success: false})
     } catch (e) {
-      res.unauthorized({success: false,error: e.message})
+      res.unauthorized({success: false, error: e.message})
     }
   }
 }
--- app/Http/api.js
+++ app/Http/api.js
@@ -17,18 +17,14 @@
 
 const Route = use('Route')
 
-Route.group('api', ()=>{
-
+Route.group('api', () => {
   Route.post('/auth', 'AuthController.login')
-
   Route.get('/article', 'ArticleController.index')
   Route.get('/article/:id', 'ArticleController.show')
   Route.post('/article', 'ArticleController.store')
   Route.put('/article/:id', 'ArticleController.update')
   Route.delete('/article/:id', 'ArticleController.destroy')
-
   Route.get('/user', 'UserController.index')
   Route.get('/user/:id', 'UserController.show')
   Route.delete('/user/:id', 'UserController.destroy')
-
 }).prefix('api/v1')
