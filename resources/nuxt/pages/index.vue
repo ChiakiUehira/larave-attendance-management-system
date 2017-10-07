0a9bdc79dd5897de5e0a64fb58bbf83a0638978a
chanu <yuyutyanu@yahoo.co.jp>
2017-10-07 14:01:46 +0900

    create http plugin

--- resources/nuxt/pages/index.vue
+++ resources/nuxt/pages/index.vue
@@ -1,25 +1,16 @@
 <template>
   <section class="container">
-    <my-article v-for="article in articles" :article="article" :key="article.id"/>
+
   </section>
 </template>
 
 <script>
-  import MyArticle from '~/components/Article.vue'
   export default {
-    components:{
-      MyArticle
-    },
-    computed:{
-      articles(){
-        return this.$store.state.articles
-      }
-    },
-    async fetch ({app, store, params}) {
-      if (!store.state.articles) {
-        const { data } = await app.$http.get('/article')
-        store.commit('setArticles', data.articles)
-      }
+    created(){
+      this.$http().post('/login',{email:'sisukai2017@gmail.com', password:'pw'}).then((obj) =>{
+        this.$store.commit('SET_TOKEN', obj.data.token)
+        this.$http().get('/news')
+      })
     }
   }
 </script>
