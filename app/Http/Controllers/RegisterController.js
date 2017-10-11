98c128f2a193a713f30580746365fcb94101fc08
chanu <yuyutyanu@yahoo.co.jp>
2017-10-11 11:53:09 +0900

    #5

--- app/Http/Controllers/RegisterController.js
+++ app/Http/Controllers/RegisterController.js
@@ -4,10 +4,13 @@ const User = use('App/Model/User')
 class RegisterController {
 
   * index (req, res) {
+    //todo urlについてるトークン検証
+    //todo urlについてるuser_id からデータをとってきて返す
     yield res.sendView('register')
   }
 
   * store (req, res) {
+    //todo 本登録する
     res.redirect('/login')
   }
 }
