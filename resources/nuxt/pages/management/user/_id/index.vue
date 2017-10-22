ab27bd2d173fa6a66f691afbbaae12ec0abcaed9
chanu <yuyutyanu@yahoo.co.jp>
2017-10-22 14:34:17 +0900

    fix #58

---
+++ resources/nuxt/pages/management/user/_id/index.vue
@@ -0,0 +1,174 @@
+<template>
+    <div>
+        <contents-name name="ユーザ詳細" />
+        <div class="page">
+            <div class="contents">
+                <div class="image">
+                    <img :src="user.thumbnail" alt="">
+                </div>
+                <div class="profile">
+                    <div class="rows">
+                        <div class="row">
+                            <div>名前</div>
+                            <div>{{fullname}}</div>
+                        </div>
+                        <div class="row">
+                            <div>名前 (かな)</div>
+                            <div>{{fullnameKana}}</div>
+                        </div>
+                        <div class="row">
+                            <div>住所</div>
+                            <div>{{address}}</div>
+                        </div>
+                        <div class="row">
+                            <div>生年月日</div>
+                            <div>{{birthday}}</div>
+                        </div>
+                        <div class="row">
+                            <div>性別</div>
+                            <div>{{gender}}</div>
+                        </div>
+                        <div class="row">
+                            <div>メールアドレス</div>
+                            <div>{{email}}</div>
+                        </div>
+                        <div class="row">
+                            <div>電話番号</div>
+                            <div>{{tel}}</div>
+                        </div>
+                        <div class="row">
+                            <div>役職</div>
+                            <div>{{position}}</div>
+                        </div>
+                        <div class="row">
+                            <div>権限</div>
+                            <div>{{authority}}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="btns">
+                    <div>
+                        <nuxt-link :to="`/management/user/${user.id}/edit`"><el-button type="primary" icon="edit"></el-button></nuxt-link>
+                        <el-button type="primary" icon="delete" @click="open" class="delete"></el-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  import ContentsName from '@/components/ContentsName.vue'
+  import moment from 'moment'
+  export default {
+    components: {
+      ContentsName
+    },
+    computed: {
+      fullname () {
+        return `${this.user.last_name} ${this.user.first_name}`
+      },
+      fullnameKana () {
+        return `${this.user.last_name_kana} ${this.user.first_name_kana}`
+      },
+      position () {
+        return this.user.position
+      },
+      email () {
+        return this.user.email
+      },
+      tel () {
+        return this.user.tel
+      },
+      gender () {
+        return this.user.gender === 'male' ? '男性' : '女性'
+      },
+      authority () {
+        return this.user.manager_flag === 'manager' ? 'マネージャー' : '一般ユーザ'
+      },
+      address () {
+        return `〒${this.user.postal_code} ${this.user.address}`
+      },
+      birthday () {
+        return moment(this.user.birthday).format("YYYY年MM月DD日")
+      },
+      user () {
+        const id = this.$route.params.id
+        return this.$store.state.users.find(user => user.id === Number(id))
+      },
+    },
+    methods:{
+      open () {
+        this.$confirm('アカウントが停止します','ユーザを退会させますか？', {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          this.$http.delete(`/user/${this.$route.params.id}`).then(({data}) => {
+            if(data.success){
+              this.$notify.success('退会が完了しました')
+              this.fetchUser()
+              this.$router.push('/management/user')
+            }else{
+              this.$notify.error('退会に失敗しました')
+            }
+          })
+        }).catch(() => {
+          this.$message({type: 'warning',message: 'キャンセルしました'});
+        });
+      },
+      async fetchUser(){
+        const { data } = await this.$http.get('user')
+        this.$store.commit('SET_USERS', data.users)
+      }
+    },
+    async fetch({app,store}){
+      if (!store.state.users) {
+        const { data } = await app.$http.get('/user')
+        store.commit('SET_USERS', data.users)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .delete{
+        margin-left:10px;
+    }
+    .page {
+        border-radius: 2px;
+    }
+    .image {
+        width: 300px;
+        padding: 25px;
+        display: inline-block;
+        background: #fff;
+        vertical-align: top;
+    }
+    .profile {
+        margin-left: 10px;
+        display: inline-block;
+        width: calc(100% - 300px - 10px);
+        vertical-align: top;
+    }
+    .profile .row {
+        background: #fff;
+        letter-spacing: 1px;
+    }
+    .profile .row:not(:last-child) {
+        margin-bottom: 10px;
+    }
+    .profile .row div {
+        padding: 10px;
+    }
+    .profile .row div:first-child {
+        font-size: 12px;
+        background: #efefef;
+    }
+    .profile .row div:last-child {
+        padding: 15px 10px;
+    }
+    .btns {
+        margin-top: 10px;
+        text-align: right;
+    }
+</style>
--- resources/nuxt/pages/management/user/index.vue
+++ resources/nuxt/pages/management/user/index.vue
@@ -1,59 +1,202 @@
 <template>
-    <section>
-        <div class="management-item">
-            <nuxt-link to="/management/user/invite">
-                <el-card>
-                    <p>招待</p>
-                </el-card>
-            </nuxt-link>
+    <div>
+        <contents-name name="ユーザ一覧"/>
+        <div class="page">
+            <div class="column-2">
+                <div class="info">
+                    <p>ユーザ数: 100</p>
+                    <p>アクティグユーザ数: 100</p>
+                    <p>アクティグユーザ数: 100</p>
+                    <nuxt-link to="/management/user/invite">
+                        <el-button icon="message" class="invite"></el-button>
+                    </nuxt-link>
+                </div>
+                <div class="controller">
+                    <el-form label-width="120px">
+                        <el-form-item label="ユーザ検索">
+                            <el-autocomplete
+                                    class="inline-input"
+                                    v-model="search.word"
+                                    :fetch-suggestions="querySearch"
+                                    placeholder="name"
+                                    :trigger-on-focus="false"
+                                    @select="handleSelect"
+                                    icon="search"
+                            ></el-autocomplete>
+                        </el-form-item>
+                        <el-form-item label="グループ">
+                            <el-select v-model="search.group" placeholder="グループ">
+                                <el-option label="選択なし" value=""></el-option>
+                                <el-option v-for="group in toValueFromGroups" :label="group.label" :value="group.value"
+                                           :key="group.id"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="アクティブ">
+                            <el-switch on-text="" off-text="" v-model="search.active"></el-switch>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </div>
+            <div class="contents">
+                <div v-if="displayUsers.length">
+                    <div class="user-warp" v-for="user in displayUsers" :key="user.id">
+                        <user-card :user="user" type="management"/>
+                    </div>
+                </div>
+                <div v-else>
+                    <div class="inner">
+                        hoge
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="management-item">
-            <nuxt-link to="/management/user/edit">
-                <el-card>
-                    <p>編集</p>
-                </el-card>
-            </nuxt-link>
-        </div>
-        <div class="management-item">
-            <nuxt-link to="/management/user/delete">
-                <el-card>
-                    <p>退社</p>
-                </el-card>
-            </nuxt-link>
-        </div>
-    </section>
+    </div>
 </template>
 
+<script>
+  import UserCard from '@/components/UserCard.vue'
+  import ContentsName from '@/components/ContentsName.vue'
+  export default {
+    data () {
+      return {
+        search: {
+          word: '',
+          group: '',
+          active: ''
+        }
+      }
+    },
+    components: {
+      UserCard,
+      ContentsName
+    },
+    computed: {
+      groups () {
+        return this.$store.state.groups
+      },
+      toValueFromGroups () {
+        const groups = this.$store.state.groups
+        return groups.map((group) => {
+          return {
+            value: group.id,
+            label: group.name
+          }
+        })
+      },
+      users () {
+        return this.$store.state.users
+      },
+      displayUsers () {
+        let users = this.$store.state.users
+
+        users = users.filter((user) => {
+          const fullName = this.fullName(user.first_name, user.last_name)
+          return fullName.indexOf(this.search.word) >= 0
+        })
+
+        users = users.filter((user) => {
+          return this.search.group ? user.group.id === this.search.group : true
+        })
+
+        return users
+      },
+      toValueFormUsers () {
+        const users = this.$store.state.users
+        return users.map((user) => {
+          return {
+            value: this.fullName(user.first_name, user.last_name),
+            nameKana: this.fullName(user.first_name_kana, user.last_name_kana)
+          }
+        })
+      }
+    },
+    methods: {
+      fullName (first, last) {
+        return `${last}${first}`
+      },
+      querySearch(queryString, cb) {
+        let results = this.toValueFormUsers
+        if (queryString) {
+          results = this.toValueFormUsers.filter((user) => {
+            return (user.value.indexOf(queryString) >= 0 || user.nameKana.indexOf(queryString) >= 0)
+          })
+        }
+        if (results.length) {
+          return cb(results)
+        }
+        return cb(this.toValueFormUsers)
+      },
+      handleSelect(user) {
+        this.search.word = user.value
+      }
+    },
+    async fetch({app, store}){
+      if (!store.state.users) {
+        const {data} = await app.$http.get('/user')
+        store.commit('SET_USERS', data.users)
+      }
+      if (!store.state.groups) {
+        const {data} = await app.$http.get('/group')
+        store.commit('SET_GROUPS', data.groups)
+      }
+    }
+  }
+</script>
+
 <style scoped>
-    .management-item {
-        width: 49%;
+    .invite{
+        margin-top:30px;
+    }
+    .info {
+        border-radius: 2px;
+        background-color: #fff;
+        padding: 30px;
+        margin-bottom: 10px;
+        width: calc(100% - 10px - 380px);
+        margin-right: 10px;
         display: inline-block;
-        margin-bottom: 25px;
-        box-sizing: border-box;
-        -webkit-transition: ease .3s;
-        -moz-transition: ease .3s;
-        transition: ease .3s;
-        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .12), 0 0 6px 0 rgba(0, 0, 0, .04);
-        border-radius: 4px;
+        vertical-align: top;
+        height: 200px;
+        color: #48576a;
     }
 
-    .management-item:hover {
-        box-shadow: 0 6px 12px 0 rgba(0, 0, 0, .12), 0 0 6px 0 rgba(0, 0, 0, .04);
+    .controller {
+        border-radius: 2px;
+        background-color: #fff;
+        padding: 23px 10px 2px;
+        margin-bottom: 10px;
+        width: 380px;
+        display: inline-block;
+        vertical-align: top;
     }
 
-    .management-item:nth-child(2n) {
-        margin-left: 2%;
+    .contents {
+        border-radius: 2px;
+        background-color: #fff;
+        padding: 30px;
     }
 
-    .el-card {
-        width: 100%;
-        padding: 60px 0px;
+    .user-warp {
+        width: 32%;
+        display: inline-block;
     }
 
-    .el-card p {
-        text-align: center;
-        font-size: 20px;
-        letter-spacing: 2px;
-        color: #8a8a8a;
+    .user-warp:not(:nth-child(3n)) {
+        margin-right: 2%;
+    }
+
+    @media screen and (max-width: 1440px) {
+        .user-warp {
+            width: 49%;
+        }
+
+        .user-warp:not(:nth-child(3n)) {
+            margin-right: 0;
+        }
+
+        .user-warp:not(:nth-child(2n)) {
+            margin-right: 2%;
+        }
     }
 </style>
+
