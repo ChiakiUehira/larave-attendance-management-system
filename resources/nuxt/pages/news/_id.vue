c5e867a5549d4f0fb8a919cb0a90aa01238e77aa
chanu <yuyutyanu@yahoo.co.jp>
2017-10-24 11:52:08 +0900

    update

--- resources/nuxt/pages/news/_id.vue
+++ resources/nuxt/pages/news/_id.vue
@@ -2,13 +2,15 @@
 <div>
   <contents-name :name="`ニュース一覧 / ${news.title}`" />
     <div class="page">
-      <div class="contents">
-        {{news.detail}}
+      <div class="contents markdown-body">
+        <div v-html="detail"></div>
       </div>
     </div>
 </div>
 </template>
 <script>
+import marked from 'marked'
+import mdCss from 'github-markdown-css'
 import ContentsName from '@/components/ContentsName.vue'
 export default {
   components: {
@@ -19,6 +21,9 @@ export default {
       const id = this.$route.params.id
       return this.$store.state.news.find(news => news.id === Number(id))
     },
+    detail (){
+      return marked(this.news.detail)
+    }
   },
   async fetch ({app, store}) {
     if (!store.state.news) {
@@ -34,5 +39,12 @@ export default {
     border-radius: 2px;
     background-color: #fff;
     padding: 30px;
+  }
+  .markdown-body {
+    color: #334257;
+    box-sizing: border-box;
+    min-width: 100%;
+    max-width: 100%;
+    margin: 0 auto;
   }
 </style>
