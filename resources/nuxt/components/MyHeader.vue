1dacb42e79d0f1d05f882cbeabd683e82fdf7a80
in-the-box <hacoco345@gmail.com>
2017-10-24 23:25:38 +0900

    fix #81

--- resources/nuxt/components/MyHeader.vue
+++ resources/nuxt/components/MyHeader.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <header>
-            <div class="beforePage" @click="beforePage"><i class="el-icon-caret-left"></i></div>
             <div class="inner">
                 <h1>{{company.company_name}}</h1>
             </div>
@@ -23,31 +22,11 @@
       company () {
         return this.$store.state.company
       }
-    },
-    methods:{
-      beforePage(){
-        this.$router.go(-1)
-      }
     }
   }
 </script>
 
 <style scoped>
-    .beforePage {
-        padding: 20px 25px;
-        background-color:#b2bfcd ;
-        color:#1F2D3D;
-        cursor: pointer;
-        transition: .3s;
-        position: absolute;
-        top:0;
-        left:0;
-        display: inline;
-    }
-
-    .beforePage:hover {
-        background-color: #8492A6;
-    }
     header {
         background-color: #eeeeee;
         width: 100%;
@@ -57,7 +36,6 @@
         padding: 19px;
     }
     header h1 {
-        padding-left:65px;
         color: #334257;
         font-size: 18px;
         font-weight: bold;
