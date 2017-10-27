c5fa02b5585d5cf73a83cf5b1d757f7d082bd3e6
in-the-box <hacoco345@gmail.com>
2017-10-27 22:29:52 +0900

    fix Procfile

--- config/cors.js
+++ config/cors.js
@@ -10,7 +10,7 @@ module.exports = {
   | define multiple origins as string.
   |
   */
-  origin: false,
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
