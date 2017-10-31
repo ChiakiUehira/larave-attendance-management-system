c2284dc24d87cb88db403ce799e9b259c7fdaedd
in-the-box <hacoco345@gmail.com>
2017-11-01 00:48:03 +0900

    add Pagination

--- resources/nuxt/pages/news/index.vue
+++ resources/nuxt/pages/news/index.vue
@@ -6,19 +6,23 @@
       </el-breadcrumb>
     </contents-name>
   <div class="page">
-    <div class="controllers">
-      <div class="btns">
-        <el-button-group>
-          <el-button type="primary" icon="arrow-left">Previous Page</el-button>
-          <el-button type="primary">Next Page<i class="el-icon-arrow-right el-icon-right"></i></el-button>
-        </el-button-group>
-      </div>
-    </div>
     <div class="contents">
-      <div v-for="item in news" :key="item.id" >
+      <div v-for="item in displayNews" :key="item.id" >
         <nuxt-link :to="`/news/${item.id}`">
           <news-card :news="item"/>
         </nuxt-link>
+      </div>
+    </div>
+    <div class="controllers">
+      <div class="btns">
+          <el-pagination
+            layout="prev, pager, next"
+            :total="maxCount"
+            :page-size="per"
+            :current-page="p"
+            @current-change="handleCurrentChange"
+          >
+          </el-pagination>
       </div>
     </div>
   </div>
@@ -35,6 +39,31 @@ export default {
   computed: {
     news () {
       return this.$store.state.news
+    },
+    p () {
+      return this.$route.query.p ? Number(this.$route.query.p) : 1
+    },
+    per () {
+      return 5
+    },
+    offset () {
+      return (this.p - 1) * this.per
+    },
+    maxCount () {
+      return this.$store.state.news.length
+    },
+    maxPage () {
+      return Math.ceil(this.maxCount / this.per)
+    },
+    displayNews () {
+      return this.news.slice(this.offset, this.offset + this.per)
+    },
+  },
+  methods: {
+    handleCurrentChange (page) {
+      return this.$router.push({
+        query: { p: page }
+      })
     }
   },
   async fetch ({app, store}) {
@@ -53,8 +82,8 @@ export default {
     padding: 30px 30px 1px;
   }
   .controllers {
-    margin-bottom: 10px;
-    text-align: right;
+    margin-top: 10px;
+    text-align: center;
   }
   .btns {
     background-color: #fff;
